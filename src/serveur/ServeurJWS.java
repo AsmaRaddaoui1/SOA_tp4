d69018a3a8dc7e1cf1b5cf25d5788a3cd3bc9025
AsmaRaddaoui1 <asma.raddaoui18@gmail.com>
2023-11-05 01:15:18 +0100

    tp4

--- src/serveur/ServeurJWS.java
+++ src/serveur/ServeurJWS.java
@@ -1,17 +1,14 @@
 package serveur;
 
+import jakarta.xml.ws.Endpoint;
+import service.CalculatriceWS;
+
 public class ServeurJWS {
-	public static void main(String[] args) {
-    // Créez une instance de la classe CalculatriceWS (le service web que nous avons créé précédemment).
-    CalculatriceWS calculatrice = new CalculatriceWS();
-    
-    // Spécifiez l'URL où vous souhaitez publier le service.
-    String url = "http://localhost:8084/calculatrice";
+    public static void main(String[] args) {
+        CalculatriceWS calculatrice =new CalculatriceWS();
+        String url ="http://localhost:8084/";
+        Endpoint.publish(url, calculatrice);
+        System.out.println("Server url:"+ url);
 
-    // Publiez le service web à l'URL spécifiée.
-    Endpoint.publish(url, calculatrice);
-
-    System.out.println("Service web CalculatriceWS publié à l'URL : " + url);
-}
-
+    }
 }
